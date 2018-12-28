7ea034d01b18f5c74ca820ac73499cf6ba3df706
DanielRabasco <danielrabascogarcia@gmail.com>
2018-12-28 19:55:32 +0100

    first commit

---
+++ spkr-io.c
@@ -0,0 +1,80 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/version.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+void spkr_init(void) {
+	void __iomem *regs_div = ioremap(0x3F1010A4, 4);
+	void __iomem *regs_ctl = ioremap(0x3F1010A0, 4);
+
+	void __iomem *regs_gpio = ioremap(0x3f200100, 4);
+	uint32_t val_gpio = ioread32(regs_gpio); 
+
+	// region for the pwm control
+	void __iomem *reg_pwm_ctl = ioremap(0x3f20C000, 4);
+	uint32_t val_pwm_ctl = ioread32(reg_pwm_ctl);
+
+	// region for the pwm range
+	void __iomem *reg_pwm_rng = ioremap(0x3f20C010, 4);
+	uint32_t val_pwm_rng = ioread32(reg_pwm_rng);
+
+	// region for the pwm data
+	void __iomem *reg_pwm_data = ioremap(0x3f20C014, 4);
+	uint32_t val_pwm_data = ioread32(reg_pwm_data);
+
+	// 
+	iowrite32(0x5A010000, regs_div);
+	iowrite32(0x5A000011, regs_ctl);
+
+	// Reseting the bits (23, 24 and 25) values and writing 010 in them
+	val_gpio &= ~(0x7 << 24);
+	val_gpio |= (0x2 << 24);
+	iowrite32(val_gpio, regs_gpio);
+
+	// Reseting bit 7 and writing 1 in it(in the pwm control region)
+	val_pwm_ctl &= ~(0x1 << 7);
+	val_pwm_ctl |= (0x1 << 7);
+	iowrite32(val_pwm_ctl, reg_pwm_ctl);
+
+	printk(KERN_INFO "spkr init\n");
+}
+
+void spkr_exit(void) {
+	iounmap(0x3F1010A4);
+	iounmap(0x3F1010A0);
+	iounmap(0x3f200100);
+	iounmap(0x3f20C000);
+	iounmap(0x3f20C010);
+	iounmap(0x3f20C014);
+
+	printk(KERN_INFO "spkr exit\n");
+}
+
+void spkr_set_frequency(unsigned int frequency) {
+	printk(KERN_INFO "spkr set frequency: %d\n", frequency);
+}
+
+void spkr_on(void) {
+	
+	// region for the pwm control
+	void __iomem *reg_pwm_ctl = ioremap(0x3f20C000, 4);
+	uint32_t val_pwm_ctl = ioread32(reg_pwm_ctl);
+
+	// Reseting bit 0 and writing 1 in it(in the pwm control region) to activate
+	val_pwm_ctl &= ~(0x1 << 0);
+	val_pwm_ctl |= (0x1 << 0);
+	iowrite32(val_pwm_ctl, reg_pwm_ctl);
+	printk(KERN_INFO "spkr ON\n");
+}
+void spkr_off(void) {
+	
+	// region for the pwm control
+	void __iomem *reg_pwm_ctl = ioremap(0x3f20C000, 4);
+	uint32_t val_pwm_ctl = ioread32(reg_pwm_ctl);
+
+	// Reseting bit 0 and writing 0 in it(in the pwm control region) to deactivate
+	val_pwm_ctl &= ~(0x0 << 0);
+	val_pwm_ctl |= (0x0 << 0);
+	iowrite32(val_pwm_ctl, reg_pwm_ctl);
+	printk(KERN_INFO "spkr OFF\n");
+}
---
+++ spkr-main.c
@@ -0,0 +1,89 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/version.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+#include <linux/kfifo.h>
+#include <linux/ioctl.h>
+#include <asm/uaccess.h>
+#include <linux/errno.h>
+
+extern int spkr_init(void);
+extern void spkr_exit(void);
+extern void spkr_set_frequency(unsigned int frequency);
+extern void spkr_on(void);
+extern void spkr_off(void);
+
+/*static int minor = 0;
+static int n_aperturas_writemode = 0;
+static dev_t dev;
+static struct cdev cdev;
+static struct class *clase;
+module_param(minor, int, S_IRUGO);
+
+struct info_mydev {
+	struct cdev mydev_cdev;
+};
+
+static int spkr_open(struct inode *inode, struct file *filp) {
+	struct info_mydev *info_dev = container_of(inode->i_cdev, struct info_mydev, mydev_cdev);
+	filp->private_data = info_dev;
+
+	//Check that there is only one call to open in write-mode
+	if ((filp->f_mode & FMODE_WRITE) != 0){
+		if (n_aperturas_writemode == 0){
+			n_aperturas_writemode++;
+			printk("WRITE");
+		}else{
+			return -EBUSY;
+		}
+	}
+	printk(KERN_ALERT "Funcion de apertura");
+	return 0;
+}
+
+
+static int spkr_release(struct inode *inode, struct file *filp) {
+	//If the open call was in write mode, decrease the counter and allow other write-mode open call
+	if ((filp->f_mode & FMODE_WRITE) != 0){
+		n_aperturas_writemode--;
+	}
+	printk(KERN_ALERT "Funcion de liberacion");
+	return 0;
+}
+
+static ssize_t spkr_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
+	printk(KERN_ALERT "Funcion de escritura");
+	return count;
+}
+
+static struct file_operations fops = {
+  .owner = THIS_MODULE,
+  .open = spkr_open,
+  .release = spkr_release,
+  .write = spkr_write
+};
+
+int spkr_init(void){
+	alloc_chrdev_region(&dev, minor, 1, "spkr");
+	cdev_init(&cdev, &fops);
+	cdev_add(&cdev, dev, 1);
+	clase = class_create(THIS_MODULE, "speaker");
+	device_create(clase, NULL, dev, NULL, "intspkr");
+	printk(KERN_ALERT "MAJOR:%i\n",MAJOR(dev));
+	printk(KERN_ALERT "MINOR:%i\n",MINOR(dev));
+	return 0;
+}
+
+void spkr_exit(void){
+	unregister_chrdev_region(dev, 1);
+	cdev_del(&cdev);
+	device_destroy(clase, dev);
+	class_destroy(clase);
+}
+
+module_init(spkr_init);
+module_exit(spkr_exit);*/
